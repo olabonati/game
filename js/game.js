fe366db0d4c02d4b4a64d07f10ef35854b1a3796
Ola Bonati <olabonati@gmail.com>
2023-05-26 00:25:11 +0200

    scoring when avoiding the cloud

--- js/game.js
+++ js/game.js
@@ -1,61 +1,81 @@
 class Player {
-    constructor() {
-      this.width = 4;
-      this.height = 10;
-      this.positionY =40;
-      this.positionX = 0;
-      this.DomElement = null;
-      this.createDomElement();
-    }
-  ///cteate DOM
-    createDomElement() {
-      this.newPlayerElm = document.createElement("div");
-      this.newPlayerElm.className = "player";
-      this.newPlayerElm.style.top = this.positionY + "vh";
-      this.newPlayerElm.style.left = this.positionX + "vw";
-      this.newPlayerElm.style.height = this.height + "vh";
-      this.newPlayerElm.style.width = this.width + "vw";
-      const parentElm = document.getElementById("board");
+  constructor() {
+    this.width = 4;
+    this.height = 10;
+    this.positionY =40;
+    this.positionX = 0;
+    this.score = 0;
+    this.scoreDisplay = document.getElementById("score");
+    this.updateScore();
+    this.newPlayerElm = null;
+    this.createDomElement();
+  }
+///cteate DOM
+  createDomElement() {
+    this.newPlayerElm = document.createElement("div");
+    this.newPlayerElm.className = "player";
+    this.newPlayerElm.style.top = this.positionY + "vh";
+    this.newPlayerElm.style.left = this.positionX + "vw";
+    this.newPlayerElm.style.height = this.height + "vh";
+    this.newPlayerElm.style.width = this.width + "vw";
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.newPlayerElm);
 
 
-      parentElm.appendChild(this.newPlayerElm);
 
-      /*const newImg = document.createElement('img');
-      this.newPlayerElm.appendChild(newImg);
-      newImg.setAttribute("src", "./giphy.gif");
-      newImg.style.top = this.positionY + "px";
-      this.newPlayerElm.style.height = this.height + "px";
-      newImg.style.width = this.width + "px";*/
-    }
-  
-    moveUp() {
-       if (this.positionY>0){
-      this.positionY -= 1;
-      this.newPlayerElm.style.top = this.positionY + "vh";
+   
 
-    }}
-  
-    moveDown() {
-        const boardElement = document.getElementById("board");
-        const boardHeight = boardElement.offsetHeight;
-        
-        if (this.positionY + this.height < 50) {
-          this.positionY += 1;
-          this.newPlayerElm.style.top = this.positionY + "vh";
-        }
+    /*const newImg = document.createElement('img');
+    this.newPlayerElm.appendChild(newImg);
+    newImg.setAttribute("src", "./giphy.gif");
+    newImg.style.top = this.positionY + "px";
+    this.newPlayerElm.style.height = this.height + "px";
+    newImg.style.width = this.width + "px";*/
+  }
+
+  moveUp() {
+     if (this.positionY>0){
+    this.positionY -= 1;
+    this.newPlayerElm.style.top = this.positionY + "vh";
+
+  }}
+
+  moveDown() {
+      const boardElement = document.getElementById("board");
+      const boardHeight = boardElement.offsetHeight;
+      
+      if (this.positionY + this.height < 50) {
+        this.positionY += 1;
+        this.newPlayerElm.style.top = this.positionY + "vh";
       }
-      }
+    }
+    updateScore(){
+      this.scoreDisplay.textContent = this.score;
+
+    }
+    increaseScore(amount){
+      this.score += amount;
+      this.updateScore();
+    }
+    }
+
+
+document.addEventListener('keydown', function(event) {
+  console.log("User pressed key: " + event.key);
+
+  if (event.key === "ArrowUp") {
+    player.moveUp();
+  } else if (event.key === "ArrowDown") {
+    player.moveDown();
+  }
+});
+
+document.querySelector("#start").addEventListener('click', ()=> {
+  window.location.reload();
+
+});
 
   
-  document.addEventListener('keydown', function(event) {
-    console.log("User pressed key: " + event.key);
-  
-    if (event.key === "ArrowUp") {
-      player.moveUp();
-    } else if (event.key === "ArrowDown") {
-      player.moveDown();
-    }
-  });
   ////create an obstacle
   class Clouds {
     constructor() {
@@ -98,22 +118,111 @@ const newCloud = new Clouds();
 cloudsArr.push(newCloud);
 },2000);
 
+
+
+function collisionAndScore (cloudInstance){
+  if (
+    cloudInstance.positionX < player.positionX + player.width &&
+    cloudInstance.positionX + cloudInstance.width > player.positionX &&
+    cloudInstance.positionY < player.positionY + player.height &&
+    cloudInstance.height + cloudInstance.positionY > player.positionY)
+    {
+    console.log("game over biatch");
+  }
+  if (cloudInstance.positionX <= 0) {
+    //delete current cloud
+    const parentElm = document.getElementById("board");
+    parentElm.removeChild(cloudInstance.newCloudElm);
+    cloudsArr.splice(cloudsArr.indexOf(cloudInstance), 1);
+
+    //increase score
+    player.increaseScore(1);
+ 
+  }}
+
 setInterval (() => {
     cloudsArr.forEach((cloudInstance) => {
         cloudInstance.moveLeft();
-        if (
-            cloudInstance.positionX < player.positionX + player.width &&
-            cloudInstance.positionX + cloudInstance.width > player.positionX &&
-            cloudInstance.positionY < player.positionY + player.height &&
-            cloudInstance.height + cloudInstance.positionY > player.positionY)
-            
-            console.log("game over biatch");}
+        collisionAndScore(cloudInstance);
+      });
+    
+}, 100);
+
+
+
+/*
+
+class Trash {
+  constructor() {
+    this.width = 4;
+    this.height = 15;
+    this.positionY = Math.floor(Math.random() * (70 - 50 + this.height)+8);
+    this.positionX = 47;
+    this.newTrashElm = null; 
+    this.score = 0;
+    this.createDomElement();
+
+
+  }
+  createDomElement() {
+    this.newTrashElm = document.createElement("div");
+    this.newTrashElm.className = "trash";
+    this.newTrashElm.style.top = this.positionY + "vh";
+    this.newTrashElm.style.left = this.positionX + "vw";
+    this.newTrashElm.style.height = this.height + "vh";
+    this.newTrashElm.style.width = this.width + "vw";
+
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.newTrashElm);
+
+  
+  }
+
+
+moveLeft(){
+  this.positionX -= 1;
+  this.newTrashElm.style.left = this.positionX + "vw";
+
+}
+
+}
+
+const trashArr=[];
+    ///create new trash every 5.4 sec//
+
+setInterval(()=> {
+  const newTrash = new Trash();
+  trashArr.push(newTrash);
+  },4000);
+
+  setInterval(() => {
+    trashArr.forEach((trashItem) => {
+      trashItem.moveLeft();
+    });
+  }, 100);
+  const newTrash = new Trash();
+
+  function collectingAndScore(trashItem) {
+    if (
+      trashItem.positionX < player.positionX + player.width &&
+      trashItem.positionX + trashItem.width > player.positionX &&
+      trashItem.positionY < player.positionY + player.height &&
+      trashItem.height + trashItem.positionY > player.positionY
+    ) { 
+      
+      player.increaseScore2(1);
+
+           }
+      if (trashItem.positionX <= 0) {
+        // Delete current trash item
+        const parentElm = document.getElementById("board");
+        parentElm.removeChild(trashItem.newTrashElm);
+        trashArr.splice(trashArr.indexOf(trashItem), 1);
+
+      }}
+    
+*/
+
+
 
     
-)}, 100);
-
-    
-
-
-
-
